7213c5f8e84ca242136669481009558aa48a441b
Mister-Hope <mister-hope@outlook.com>
2024-03-25 13:54:52 +0000

    Deploying to gitee from @ vuepress-theme-hope/vuepress-theme-hope@b5c864cb4d95c3f70f6d39221cf2924d25372ab4 🚀

---
+++ assets/pageview-C1CeR1zL.js
@@ -0,0 +1,7 @@
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = []
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
+import{l as o,aa as a}from"./app-oMgDuAfe.js";const i=async()=>{try{const{pageviewCount:e}=await o(()=>import("./app-oMgDuAfe.js").then(r=>r.ab),__vite__mapDeps([])),t=a();return e({serverURL:t.serverURL})}catch{console.error("@waline/client is not installed!")}};export{i as updatePageview};
